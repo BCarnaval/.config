9ddf2b99ed27f2e5881623d78f2993b28c3b28c3
BCarnaval <antoinedelagrave@hotmail.com>
2022-10-17 13:49:50 -0400

    Massive update

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -2,9 +2,11 @@ astronvim.vim_opts(astronvim.user_plugin_opts("options", {
   opt = {
     backspace = vim.opt.backspace + { "nostop" }, -- Don't stop backspace at insert
     clipboard = "unnamedplus", -- Connection to the system clipboard
+    cmdheight = 0, -- hide command line unless needed
     completeopt = { "menuone", "noselect" }, -- Options for insert mode completion
     copyindent = true, -- Copy the previous indentation on autoindenting
     cursorline = true, -- Highlight the text line of the cursor
+    expandtab = true, -- Enable the use of space in tab
     fileencoding = "utf-8", -- File content encoding for the buffer
     fillchars = { eob = " " }, -- Disable `~` on nonexistent lines
     history = 100, -- Number of commands to remember in a history table
@@ -13,32 +15,28 @@ astronvim.vim_opts(astronvim.user_plugin_opts("options", {
     lazyredraw = true, -- lazily redraw screen
     mouse = "a", -- Enable mouse support
     number = true, -- Show numberline
-    preserveindent = false, -- Preserve indent structure as much as possible
+    preserveindent = true, -- Preserve indent structure as much as possible
     pumheight = 10, -- Height of the pop up menu
     relativenumber = true, -- Show relative numberline
     scrolloff = 8, -- Number of lines to keep above and below the cursor
-
-    expandtab = true, -- Enable the use of space in tab
-    tabstop = 8, -- Number of space in a tab
-    shiftwidth = 8, -- Number of space inserted for indentation
-
+    shiftwidth = 4, -- Number of space inserted for indentation
     showmode = false, -- Disable showing modes in command line
+    showtabline = 2, -- always display tabline
     sidescrolloff = 8, -- Number of columns to keep at the sides of the cursor
     signcolumn = "yes", -- Always show the sign column
     smartcase = true, -- Case sensitivie searching
     splitbelow = true, -- Splitting a new window below the current one
     splitright = true, -- Splitting a new window at the right of the current one
     swapfile = false, -- Disable use of swapfile for the buffer
+    tabstop = 4, -- Number of space in a tab
     termguicolors = true, -- Enable 24-bit RGB color in the TUI
     timeoutlen = 300, -- Length of time to wait for a mapped sequence
     undofile = true, -- Enable persistent undo
     updatetime = 300, -- Length of time to wait before triggering the plugin
-    wrap = true, -- Disable wrapping of lines longer than the width of window
+    wrap = true, -- Enable wrapping of lines longer than the width of window
     writebackup = false, -- Disable making a backup before overwriting a file
   },
   g = {
-    do_filetype_lua = 1, -- use filetype.lua
-    did_load_filetypes = 0, -- don't use filetype.vim
     highlighturl_enabled = true, -- highlight URLs by default
     mapleader = " ", -- set leader key
     zipPlugin = false, -- disable zip
@@ -59,5 +57,9 @@ astronvim.vim_opts(astronvim.user_plugin_opts("options", {
     loaded_zipPlugin = true, -- disable zip
     loaded_vimball = true, -- disable vimball
     loaded_vimballPlugin = true, -- disable vimball
+    cmp_enabled = true, -- enable completion at start
+    autopairs_enabled = true, -- enable autopairs at start
+    diagnostics_enabled = true, -- enable diagnostics at start
+    status_diagnostics_enabled = true, -- enable diagnostics in statusline
   },
 }))
